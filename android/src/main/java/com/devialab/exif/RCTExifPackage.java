d0cced0f46aea2b5b8e235db8bb17bbbf29bceea
Ralf Nieuwenhuizen <ralfnieuwenhuizen@gmail.com>
2017-08-17 13:08:28 +0200

    Keep support for RN versions <0.47

--- android/src/main/java/com/devialab/exif/RCTExifPackage.java
+++ android/src/main/java/com/devialab/exif/RCTExifPackage.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -17,6 +18,11 @@ public class RCTExifPackage implements ReactPackage {
         return Arrays.<NativeModule>asList(
                 new Exif(reactContext)
         );
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {		
+        return Collections.emptyList();		
     }
 
     @Override
